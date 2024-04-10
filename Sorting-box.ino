c0b1a42684b6011f461b1cfe0c27dbfa355ef535
HarringtonIdahosa <81630373+HarringtonIdahosa@users.noreply.github.com>
2024-04-10 17:35:51 -0400

    Update Sorting-box.ino

--- Sorting-box.ino
+++ Sorting-box.ino
@@ -83,7 +83,7 @@ unsigned long lastInputTime = 0; // Variable to store the time of the last input
 
 void setup() {
   Serial.begin(9600); // Initialize serial communications with the PC
-  while (!Serial); // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+  while (!Serial); // Do nothing if no serial port is opened 
   SPI.begin(); // Init SPI bus
   mfrc522.PCD_Init(); // Init MFRC522 for the first reader
   
